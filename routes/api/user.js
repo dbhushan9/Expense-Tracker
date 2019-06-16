0f20f8c4c77782321a2d9babc8696c8e37889940
Bhushan Deshmukh <dbhushan912@gmail.com>
2019-06-16 10:15:25 +0530

    added basic registration and login functions

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+
+const keys = require('../../config/keys');
+const User = require('../../models/User');
 
 const router = express.Router();
 
@@ -6,5 +12,69 @@ const router = express.Router();
 //@desc test user api
 //@access Public
 router.post('/test', (req, res) => res.json({ msg: 'User API' }));
+
+//@route api/user/register
+//@desc register user
+//@access Public
+router.post('/register', (req, res) => {
+  //validate
+  const errors = {};
+  User.findOne({ username: req.body.username }).then(user => {
+    if (user) {
+      errors.username = 'username is taken';
+      return res.status(400).json(errors);
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        username: req.body.username,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
+
+//@route api/user/login
+//@desc login user
+//@access Public
+router.post('/login', (req, res) => {
+  //Validate
+  const errors = {};
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({ username }).then(user => {
+    if (user) {
+      bcrypt.compare(password, user.password).then(isMatch => {
+        if (isMatch) {
+          const payload = {
+            id: user.id,
+            username: user.username,
+            name: user.name
+          };
+          //sign payload
+          jwt.sign(payload, keys.secretOrKey, (er, token) => {
+            return res.json({ success: 'true', token: 'bearer ' + token });
+          });
+        } else {
+          errors.password = 'password is incorrect';
+          return res.status(400).json(errors);
+        }
+      });
+    } else {
+      errors.username = 'username not found';
+      return res.status(404).json(errors);
+    }
+  });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const morgan = require('morgan');
 
+const keys = require('./config/keys');
 const walletRoutes = require('./routes/api/wallet');
 const transactionRoutes = require('./routes/api/transaction');
 const userRoutes = require('./routes/api/user');
 
 const PORT = process.env.PORT || 5000;
+const db = keys.mongoURI;
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log('connected to mongodb'))
+  .catch(err => console.log(err));
+
+//logging
+app.use(morgan('dev'));
+
+//use passport
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
 app.get('/', (req, res) => res.send('Hello World'));
 
